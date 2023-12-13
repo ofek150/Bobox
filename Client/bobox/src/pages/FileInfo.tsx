1d749515237fa974ec76e9e7194a21d754e55d37
danielmordo <102387603+danielmordo@users.noreply.github.com>
2023-12-13 12:58:06 +0200

    Added fileInfo page, download btn working

--- Client/bobox/src/pages/FileInfo.tsx
+++ Client/bobox/src/pages/FileInfo.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import ShowFileInfo from '../components/ShowFileInfo';
 import { DownloadInfoParams, SharedFile } from '../utils/types';
 import { getFileInfo } from '../services/firebase';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Button } from '@mui/material';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const FileInfo: React.FC = () => {
-  const { ownerUid, fileId, downloadId } = useParams();
   const [fileInfo, setFileInfo] = useState<SharedFile | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
+    // Assuming you have a function to fetch file info based on the URL params
+    const fetchFileInfo = async () => {
+      const { ownerUid, fileId, downloadId } = useParams();
       if (!ownerUid || !fileId || !downloadId) {
         console.error('Invalid parameters');
         return;
@@ -36,8 +38,72 @@ const FileInfo: React.FC = () => {
       }
     };
 
-    fetchData();
-  }, [ownerUid, fileId, downloadId]);
+    fetchFileInfo();
+  }, []);
+
+  const downloadPart = async (partLink: string) => {
+    try {
+      const response = await axios.get(partLink, { responseType: 'arraybuffer' });
+      return response.data;
+    } catch (error) {
+      console.error('Error downloading part:', error);
+      throw error;
+    }
+  };
+
+  const downloadAndAssembleFile = async () => {
+    try {
+      if (fileInfo && fileInfo.downloadLinks && fileInfo.downloadLinks.length > 0) {
+        const maxRetries = 3;
+
+        const assembleFile = async (retryCount: number) => {
+          try {
+            // Download all parts in parallel
+            const partPromises = fileInfo.downloadLinks.map(async (partLink) => {
+              try {
+                return await downloadPart(partLink);
+              } catch (downloadError) {
+                // Retry if download fails, up to maxRetries times
+                if (retryCount < maxRetries) {
+                  console.warn(`Retrying download part after failure (${retryCount + 1} of ${maxRetries})`);
+                  return await assembleFile(retryCount + 1);
+                } else {
+                  throw downloadError;
+                }
+              }
+            });
+
+            const parts = await Promise.all(partPromises);
+
+            // Concatenate parts into a single ArrayBuffer
+            const assembledFile = new Uint8Array(
+              parts.reduce((acc, part) => [...acc, ...new Uint8Array(part)], [])
+            );
+
+            // Convert the ArrayBuffer to a Blob
+            const assembledBlob = new Blob([assembledFile]);
+
+            // Create a download link and trigger the download
+            const downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(assembledBlob);
+            downloadLink.download = 'assembledFile.dat'; // Specify the filename
+            downloadLink.click();
+          } catch (error) {
+            console.error('Error assembling file:', error);
+          }
+        };
+
+        await assembleFile(0);
+      }
+    } catch (error) {
+      console.error('Error downloading and assembling file:', error);
+    }
+  };
+
+  const handleDownload = async () => {
+    // Call the function to initiate the download and assembly process
+    await downloadAndAssembleFile();
+  };
 
   if (loading) {
     return <CircularProgress />;
@@ -48,14 +114,12 @@ const FileInfo: React.FC = () => {
     return <>Error</>;
   }
 
-  const handleDownload = async () => {
-    
-  }
   return (
     <>
       <ShowFileInfo {...fileInfo} />
-      
-      <button onClick={handleDownload}>Download file</button>
+      <Button variant="contained" color="primary" onClick={handleDownload}>
+        Download File
+      </Button>
     </>
   );
 };
